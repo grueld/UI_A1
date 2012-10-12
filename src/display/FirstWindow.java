1778629d0e8f4a1456f19022ff43887e80126cce
grueld <tonton_38@hotmail.fr>
2012-10-12 19:03:22 -0400

    où est passer charly

--- src/display/FirstWindow.java
+++ src/display/FirstWindow.java
@@ -46,13 +46,14 @@ public class FirstWindow extends JFrame {
 		label1.setFont (new Font("Courier", Font.BOLD, 18));  //on choisi le format du text
 		un_jpanel.add (label1) ;                              //ensuite on doit l'ajouter à un JPanel
 		 */
-		
+
 		// INITIALISATION DES BOUTONS
 		for ( int i = 0 ; i < tab_string_control.length ; i++ ) {
 			tab_button_control[i] = new JButton(tab_string_control[i]);
-			tab_button_control[i].setPreferredSize(dim);
+			tab_button_control[i].setPreferredSize(dim) ;   //configure le choix de la dimension
 			tab_button_control[i].addActionListener (new BoutonListener()) ;  // on ajoute un listener qui permet d'écouter le clic
-			tab_button_control[i].setEnabled (false) ;
+			tab_button_control[i].setEnabled(true) ;     // permet de rendre utilisable ou non un bouton
+			tab_button_control[i].setFont(font) ;        // configure un format pour le texte du bouton
 			Boutons.add (tab_button_control[i]) ;  // on ajoute le bouton à un JPanel pour l'afficher
 		}
 
@@ -72,13 +73,6 @@ public class FirstWindow extends JFrame {
 		//p2.repaint();
 	}
 
-	public void enableButtons() {
-		for (JButton bouton: tab_button_control) {
-			bouton.setEnabled(true) ;     // permet de rendre utilisable ou non un bouton
-			bouton.setFont(font) ;        // configure un format pour le texte du bouton
-		}
-	}
-
 	/**
 	 * Ecoute les clics sur les boutons
 	 */
@@ -86,20 +80,18 @@ public class FirstWindow extends JFrame {
 
 		public void actionPerformed(ActionEvent arg0) {
 			// if (arg0.getSource() == tab_button_control[0]) {     // permet d'identitifier quel bouton
-				tab_button_control[0].setEnabled(false) ;
-				tab_button_control[1].setEnabled(true) ;
-				tab_button_control[2].setEnabled(true) ;
-			}
-			else if (arg0.getSource() == tab_button_control[3]) {    // graph
-				double [] tab = new double[3] ;
-				tab_button_control[3].setEnabled(false) ;
-				GraphingData graph = new GraphingData( tab ) ;
-				container.add(graph, BorderLayout.CENTER) ;
-				enableButtons() ;
-				for (JButton bouton: tab_button_control) { bouton.setEnabled(false) ;}
-			}
+			tab_button_control[0].setEnabled(false) ;
+			tab_button_control[1].setEnabled(true) ;
+
+			double [] tab = new double[3] ;
+			tab_button_control[3].setEnabled(false) ;
+			GraphingData graph = new GraphingData( tab ) ;
+			container.add(graph, BorderLayout.CENTER) ;
+			enableButtons() ;
+			for (JButton bouton: tab_button_control) { bouton.setEnabled(false) ;}
 		}
 	}
+
 
 	/**
 	 * Ecoute les boutons de choix des strategies
