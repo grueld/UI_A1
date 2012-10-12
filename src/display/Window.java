60add541c47d962b1364dc0703588b5262e90624
Serge <sergenhs@hotmail.com>
2012-10-12 18:03:07 -0400

    added a comment for testing

--- src/display/Window.java
+++ src/display/Window.java
@@ -3,6 +3,7 @@ package display;
 public class Window {
 	
 	public static void main(String[] args) {
+		// test comment
 		Thread.currentThread().setName("Thread principal") ;
 		FirstWindow sim = new FirstWindow() ;
 	}
