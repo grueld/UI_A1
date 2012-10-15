2415e71841bb3d4ea622dfa6beb83aac9dc54cdb
Ludo <ludovic.lavalette@gmail.com>
2012-10-15 18:52:56 -0400

    conversion greyscale

--- src/display/ColorPanel.java
+++ src/display/ColorPanel.java
@@ -8,33 +8,38 @@ public class ColorPanel extends JPanel implements ActionListener
 {
 	private JButton chooserButton;
 	private Color chosenColor;
+	private Color chosenColorGrey;
 	private JPanel colorPanel;
 	private JPanel greyPanel;
 	
 	public ColorPanel()
 	{
 		super();
-	   chooserButton = new JButton("Choose Color");
-	   colorPanel = new JPanel();
-	   greyPanel = new JPanel();
-	   chooserButton.addActionListener(this);
-	   setLayout(new GridLayout(3, 1));
-	   setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
-	   add(chooserButton);
-	   add(colorPanel);
-	   add(greyPanel);
-   }
+		chooserButton = new JButton("Choose Color");
+		colorPanel = new JPanel();
+		greyPanel = new JPanel();
+		chooserButton.addActionListener(this);
+		setLayout(new GridLayout(3, 1));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
+		add(chooserButton);
+		add(colorPanel);
+		add(greyPanel);
+	}
 	
-   public void actionPerformed(ActionEvent ae)
-   {
-      if (ae.getSource() == chooserButton)
-      {
-    	  Color tmp = JColorChooser.showDialog(this,"Choose color",this.getBackground());
-    	  if (tmp != null)
-    	  {
-    		  chosenColor = tmp;
-    		  colorPanel.setBackground(chosenColor);
-    	  }
-      }
-   }
+	public void actionPerformed(ActionEvent ae)
+	{
+	   if (ae.getSource() == chooserButton)
+	   {
+		   Color tmp = JColorChooser.showDialog(this,"Choose color",this.getBackground());
+		   if (tmp != null)
+		   {
+			   chosenColor = tmp;
+			   colorPanel.setBackground(chosenColor);
+			   //Greyscale
+			   int GreyComponent = (int) (0.21*tmp.getRed() + 0.71*tmp.getGreen() + 0.07*tmp.getBlue());
+			   chosenColorGrey = new Color (GreyComponent, GreyComponent, GreyComponent);
+			   greyPanel.setBackground(chosenColorGrey);
+		   }
+	   }
+	}
 }
