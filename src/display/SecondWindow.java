8c477f38803d31a07f95f174cc200cf2b0f906cb
grueld <tonton_38@hotmail.fr>
2012-10-18 18:55:27 -0400

    solution pour éviter la liste

--- src/display/SecondWindow.java
+++ src/display/SecondWindow.java
@@ -8,6 +8,7 @@ public class SecondWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel contentPane;
+	private int nbrOfColors ;
 	// private List<ColorPanel> colorList= new ArrayList<ColorPanel> () ; not needed for remove feature
 	
 	public static void main(String[] args) {
@@ -29,17 +30,18 @@ public class SecondWindow extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public SecondWindow(int nbrOfColors) {
+	public SecondWindow(int n) {
+		nbrOfColors = n ;
 		contentPane = new JPanel();
 		contentPane.setLayout(new FlowLayout());
 		setContentPane(contentPane);
 		setTitle("Your colors");
 		setSize(120 * nbrOfColors, 100);              	         
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		initComponents(nbrOfColors);
+		initComponents();
 	}
 
-	public void initComponents(int nbrOfColors) {
+	public void initComponents() {
 		for (int i = 0; i < nbrOfColors ; i++) {
 			contentPane.add(new ColorPanel(this.contentPane));
 			//ColorPanel pan = new ColorPanel(this.contentPane) ;
@@ -48,6 +50,12 @@ public class SecondWindow extends JFrame {
 		// for (Object p : colorList) contentPane.add((JPanel) p) ; not needed for remove feature
 	}
 	
+	
+	public void remove (Component comp) {
+		nbrOfColors-- ;
+		this.remove(comp) ;
+		this.pack() ;
+	}
 	// no need to use this method since the remove feature is implemented in the 
 	// action listener of class ColorPanel
 	/*public void deleteColorPanel (int id) {
