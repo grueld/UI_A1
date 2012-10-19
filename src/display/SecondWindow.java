ce8d3e7064f1141b9b5bbf3f0bb49ddd87709f24
grueld <tonton_38@hotmail.fr>
2012-10-18 21:20:03 -0400

    enable/disable errors

--- src/display/SecondWindow.java
+++ src/display/SecondWindow.java
@@ -2,7 +2,6 @@ package display;
 
 import javax.swing.*;
 import java.awt.*;
-//import java.util.*;
 
 public class SecondWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -10,13 +9,12 @@ public class SecondWindow extends JFrame {
 	private JPanel contentPane;
 	private int nbrOfColors ;
 	private AddPanel a ;
-	// private List<ColorPanel> colorList= new ArrayList<ColorPanel> () ; not needed for remove feature
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					SecondWindow frame = new SecondWindow(3);
+					SecondWindow frame = new SecondWindow(2);
 					frame.setLocation (200,200) ;
 					frame.pack();
 					frame.setVisible(true);
@@ -28,15 +26,12 @@ public class SecondWindow extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public SecondWindow(int n) {
 		nbrOfColors = n ;
 		contentPane = new JPanel();
 		contentPane.setLayout(new FlowLayout()) ;
 		setContentPane(contentPane) ;
-		setTitle("Your " + nbrOfColors + "colors");
+		title() ;
 		setSize(120 * nbrOfColors, 5666660) ;              	         
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		initComponents();
@@ -45,18 +40,24 @@ public class SecondWindow extends JFrame {
 	public void initComponents() {
 		for (int i = 0; i < nbrOfColors ; i++) {
 			contentPane.add(new ColorPanel(this));
-			//ColorPanel pan = new ColorPanel(this.contentPane) ;
-			// colorList.add(pan) ;   not needed for remove feature
 		}
 		a = new AddPanel(this) ;
 		contentPane.add(a) ;
-		// for (Object p : colorList) contentPane.add((JPanel) p) ; not needed for remove feature
+		if (nbrOfColors == 2) {
+			ColorPanel c = (ColorPanel) getContentPane().getComponent(0) ;
+			c.deleteButton.setEnabled(false) ;
+			c = (ColorPanel) getContentPane().getComponent(1) ;
+			c.deleteButton.setEnabled(false) ;
+		}
+		else if (nbrOfColors == 10) {
+			a.addButton.setEnabled(false) ;
+		}
 	}
 	
 	public void removePanel (Component comp) {
 		this.remove(comp) ;
 		nbrOfColors-- ;
-		setTitle("Your " + nbrOfColors + " colors");
+		title() ;
 		if (nbrOfColors == 2) {
 			ColorPanel c = (ColorPanel) getContentPane().getComponent(0) ;
 			c.deleteButton.setEnabled(false) ;
@@ -71,7 +72,7 @@ public class SecondWindow extends JFrame {
 	
 	public void addColorPanel() {
 		nbrOfColors++ ;
-		setTitle("Your " + nbrOfColors + "colors") ;
+		title() ;
 		super.remove(a) ;
 		contentPane.add(new ColorPanel(this)) ;
 		add(a) ;
@@ -86,5 +87,8 @@ public class SecondWindow extends JFrame {
 		}
 		pack() ;
 	}
-
+ 
+	private void title () {
+		setTitle("Your " + nbrOfColors + " colors") ;
+	}
 }
